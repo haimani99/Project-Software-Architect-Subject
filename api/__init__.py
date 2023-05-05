23a3cfc991b76bacbc472bf48afe4e4e485ec404
haimani99 <130327689+haimani99@users.noreply.github.com>
2023-05-06 01:11:32 +0500

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 CORS(app)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
 app.config['SECRET_KEY'] = 'secretkey'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)

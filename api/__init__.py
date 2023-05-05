e88e3beffe9072694d9981275f8ddb3e7c3ab82a
haimani99 <130327689+haimani99@users.noreply.github.com>
2023-05-06 01:10:41 +0500

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,19 +1,18 @@
-from api import views
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 
 CORS(app)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SECRET_KEY'] = 'secretkey'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
 
 db = SQLAlchemy(app)
-db.init_app(app)
 bcrypt = Bcrypt(app)
